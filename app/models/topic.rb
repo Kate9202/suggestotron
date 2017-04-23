f6909e83d43600b79d5f50a20b434fa57bb53621
Alex Chen <alex@spoonrocket.com>
2017-04-23 01:30:38 -0700

    added geocode validation

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,14 @@
 class Topic < ApplicationRecord
+  geocoded_by :address
   reverse_geocoded_by :latitude, :longitude
-  after_validation :reverse_geocode  # auto-fetch address
+  after_validation :address_coding
   has_many :votes, dependent: :destroy
+
+  def address_coding
+    if address.present?
+      geocode
+    elsif latitude.present? && longitude.present?
+      reverse_geocode
+    end
+  end
 end
